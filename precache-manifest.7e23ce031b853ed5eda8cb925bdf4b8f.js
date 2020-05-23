634da54d9352f200ae4121cc1fb17a5320c3eee9
patzila <patzila@gmail.com>
2020-05-23 13:06:00 -0400

    Updates

---
+++ precache-manifest.7e23ce031b853ed5eda8cb925bdf4b8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c1e50e09a2a0ab21e02024d752aba8",
+    "url": "/wow-gold-helper/index.html"
+  },
+  {
+    "revision": "3e0bc3f3889b96fbf727",
+    "url": "/wow-gold-helper/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "1ecde25e203803cd2731",
+    "url": "/wow-gold-helper/static/js/2.a7988e26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wow-gold-helper/static/js/2.a7988e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e0bc3f3889b96fbf727",
+    "url": "/wow-gold-helper/static/js/main.69db5817.chunk.js"
+  },
+  {
+    "revision": "9b436063e96193f5718f",
+    "url": "/wow-gold-helper/static/js/runtime-main.6087d62b.js"
+  }
+]);
